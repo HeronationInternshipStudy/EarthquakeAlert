1d5651c32881531048fb484009b79bdec70b5a42
mina <mina077424@gmail.com>
2020-01-02 10:07:27 +0900

    load manager & progress bar

--- app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
+++ app/src/main/java/com/example/android/quakereport/EarthquakeActivity.java
@@ -15,33 +15,46 @@
  */
 package com.example.android.quakereport;
 
+import android.app.LoaderManager;
+import android.content.Context;
 import android.content.Intent;
+import android.content.Loader;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import java.util.ArrayList;
+import java.util.List;
 
-public class EarthquakeActivity extends AppCompatActivity {
+public class EarthquakeActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<List<earthquake>> {
 
     public static final String LOG_TAG = EarthquakeActivity.class.getName();
+    private static final String USGS_REQUEST_URL =
+            "https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&orderby=time&minmag=6&limit=10";
+    private earthquakeAdpater adapter;
+    private static final int EARTHQUAKE_LOADER_ID=1;
+    private TextView emptyStateTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.earthquake_activity);
 
-        // Create a fake list of earthquake locations.
-        ArrayList<earthquake> earthquakes =QueryUtils.extractEarthquakes();
+        emptyStateTextView=(TextView)findViewById(R.id.empty_view);
 
         // Find a reference to the {@link ListView} in the layout
         ListView earthquakeListView = (ListView) findViewById(R.id.list);
+        earthquakeListView.setEmptyView(emptyStateTextView);
+        adapter=new earthquakeAdpater(this,new ArrayList<earthquake>());
 
-        final earthquakeAdpater adapter=new earthquakeAdpater(this,earthquakes);
 
         // Set the adapter on the {@link ListView}
         // so the list can be populated in the user interface
@@ -56,5 +69,40 @@ public class EarthquakeActivity extends AppCompatActivity {
                 startActivity(websiteIntent);
             }
         });
+
+        ConnectivityManager connMgr=(ConnectivityManager)getSystemService(Context.CONNECTIVITY_SERVICE);
+        NetworkInfo networkInfo=connMgr.getActiveNetworkInfo();
+        if(networkInfo!=null&&networkInfo.isConnected()){
+            LoaderManager loaderManager=getLoaderManager();
+            loaderManager.initLoader(EARTHQUAKE_LOADER_ID,null,this);
+        }
+        else{
+            View loadingIndicator = findViewById(R.id.loading_indicator);
+            loadingIndicator.setVisibility(View.GONE);
+            emptyStateTextView.setText(R.string.no_internet_connection);
+        }
     }
+
+    @Override
+    public Loader<List<earthquake>> onCreateLoader(int i, Bundle bundle) {
+        return new EarthquakeLoader(this,USGS_REQUEST_URL);
+    }
+
+    @Override
+    public void onLoadFinished(Loader<List<earthquake>> loader, List<earthquake> earthquakes) {
+        View loadingIndicator=findViewById(R.id.loading_indicator);
+        loadingIndicator.setVisibility(View.GONE);
+        emptyStateTextView.setText(R.string.no_earthquakes);
+        adapter.clear();
+        if(earthquakes!=null&&!earthquakes.isEmpty()){
+         adapter.addAll(earthquakes);
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<List<earthquake>> loader) {
+        adapter.clear();
+    }
+
+
 }
--- app/src/main/java/com/example/android/quakereport/QueryUtils.java
+++ app/src/main/java/com/example/android/quakereport/QueryUtils.java
@@ -1,14 +1,25 @@
 package com.example.android.quakereport;
 
+import android.text.TextUtils;
 import android.util.Log;
 
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
+import java.nio.charset.Charset;
 import java.util.ArrayList;
+import java.util.List;
 
 public final class QueryUtils {
+    public static final String LOG_TAG = EarthquakeActivity.class.getName();
 
     /** Sample JSON response for a USGS query */
     private static final String SAMPLE_JSON_RESPONSE = "{\"type\":\"FeatureCollection\",\"metadata\":{\"generated\":1462295443000,\"url\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=2016-01-01&endtime=2016-01-31&minmag=6&limit=10\",\"title\":\"USGS Earthquakes\",\"status\":200,\"api\":\"1.5.2\",\"limit\":10,\"offset\":1,\"count\":10},\"features\":[{\"type\":\"Feature\",\"properties\":{\"mag\":7.2,\"place\":\"88km N of Yelizovo, Russia\",\"time\":1454124312220,\"updated\":1460674294040,\"tz\":720,\"url\":\"http://earthquake.usgs.gov/earthquakes/eventpage/us20004vvx\",\"detail\":\"http://earthquake.usgs.gov/fdsnws/event/1/query?eventid=us20004vvx&format=geojson\",\"felt\":2,\"cdi\":3.4,\"mmi\":5.82,\"alert\":\"green\",\"status\":\"reviewed\",\"tsunami\":1,\"sig\":798,\"net\":\"us\",\"code\":\"20004vvx\",\"ids\":\",at00o1qxho,pt16030050,us20004vvx,gcmt20160130032510,\",\"sources\":\",at,pt,us,gcmt,\",\"types\":\",cap,dyfi,finite-fault,general-link,general-text,geoserve,impact-link,impact-text,losspager,moment-tensor,nearby-cities,origin,phase-data,shakemap,tectonic-summary,\",\"nst\":null,\"dmin\":0.958,\"rms\":1.19,\"gap\":17,\"magType\":\"mww\",\"type\":\"earthquake\",\"title\":\"M 7.2 - 88km N of Yelizovo, Russia\"},\"geometry\":{\"type\":\"Point\",\"coordinates\":[158.5463,53.9776,177]},\"id\":\"us20004vvx\"},\n" +
@@ -30,21 +41,75 @@ public final class QueryUtils {
     private QueryUtils() {
     }
 
-    /**
-     * Return a list of {@link earthquake} objects that has been built up from
-     * parsing a JSON response.
-     */
-    public static ArrayList<earthquake> extractEarthquakes() {
+    private static URL createUrl(String stringUrl) {
+        URL url=null;
 
-        // Create an empty ArrayList that we can start adding earthquakes to
-        ArrayList<earthquake> earthquakes = new ArrayList<>();
+            try {
+                url=new URL(stringUrl);
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            }
+            return url;
+    }
 
-        // Try to parse the SAMPLE_JSON_RESPONSE. If there's a problem with the way the JSON
-        // is formatted, a JSONException exception object will be thrown.
-        // Catch the exception so the app doesn't crash, and print the error message to the logs.
+    private static String makeHttpRequest(URL url) throws IOException{
+        String jsonResponse="";
+        if(url==null){
+            return jsonResponse;
+        }
+
+        HttpURLConnection urlConnection=null;
+        InputStream inputStream=null;
+
         try {
+            urlConnection = (HttpURLConnection) url.openConnection();
+            urlConnection.setReadTimeout(10000);
+            urlConnection.setConnectTimeout(15000);
+            urlConnection.setRequestMethod("GET");
+            urlConnection.connect();
 
-            JSONObject baseJsonResponse=new JSONObject(SAMPLE_JSON_RESPONSE);
+            if (urlConnection.getResponseCode() == 200) {
+                inputStream = urlConnection.getInputStream();
+                jsonResponse = readFromStream(inputStream);
+            } else {
+                Log.e(LOG_TAG, "Error response code: " + urlConnection.getResponseCode());
+            }
+        }catch (IOException e){
+            Log.e(LOG_TAG,"Problem retrieving the earthquake JSON results",e);
+        }finally{
+            if(urlConnection!=null){
+                urlConnection.disconnect();
+            }
+            if(inputStream!=null){
+                inputStream.close();
+            }
+        }
+        return jsonResponse;
+    }
+
+    private static String readFromStream(InputStream inputStream) throws IOException {
+        StringBuilder output=new StringBuilder();
+        if(inputStream!=null){
+            InputStreamReader inputStreamReader=new InputStreamReader(inputStream, Charset.forName("UTF-8"));
+            BufferedReader reader=new BufferedReader(inputStreamReader);
+            String line=reader.readLine();
+            while(line!=null){
+                output.append(line);
+                line=reader.readLine();
+            }
+        }
+        return output.toString();
+    }
+
+    private static List<earthquake> extractFeatureFromJson(String earthquakeJSON){
+        if(TextUtils.isEmpty(earthquakeJSON)){
+            return null;
+        }
+
+        List<earthquake> earthquakes=new ArrayList<>();
+
+        try {
+            JSONObject baseJsonResponse=new JSONObject(earthquakeJSON);
             JSONArray earthquakeArray=baseJsonResponse.getJSONArray("features");
 
             for(int i=0;i<earthquakeArray.length();i++){
@@ -55,22 +120,30 @@ public final class QueryUtils {
                 long time=properties.getLong("time");
                 String url=properties.getString("url");
 
-                earthquake earthquake_var = new earthquake(magnitude,location,time,url);
-                earthquakes.add(earthquake_var);
-
+                earthquake earthquake=new earthquake(magnitude,location,time,url);
+                earthquakes.add(earthquake);
             }
-
-            // TODO: Parse the response given by the SAMPLE_JSON_RESPONSE string and
-            // build up a list of Earthquake objects with the corresponding data.
-
         } catch (JSONException e) {
-            // If an error is thrown when executing any of the above statements in the "try" block,
-            // catch the exception here, so the app doesn't crash. Print a log message
-            // with the message from the exception.
-            Log.e("QueryUtils", "Problem parsing the earthquake JSON results", e);
+            e.printStackTrace();
+        }
+       return earthquakes;
+    }
+
+    public static List<earthquake> fetchEarthquakeData(String requestURL){
+        URL url=createUrl(requestURL);
+        String jsonResponse=null;
+        try {
+            Thread.sleep(2000);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        try {
+            jsonResponse=makeHttpRequest(url);
+        } catch (IOException e) {
+            e.printStackTrace();
         }
 
-        // Return the list of earthquakes
+        List<earthquake> earthquakes=extractFeatureFromJson(jsonResponse);
         return earthquakes;
     }
 
